23577fe20209d319e37ea8a9b88282848447edea
Joey Frohikey <frohikey@gmail.com>
2018-12-26 15:34:48 +0100

    Work in progress

---
+++ Oogi2.Identity.CoreWeb/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Oogi2.Attributes;
+
+namespace Oogi2.Identity.CoreWeb.Models
+{
+    [EntityType("entity", "oogi2.identity.web")]
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ Oogi2.Identity.CoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Oogi2.Identity.CoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
